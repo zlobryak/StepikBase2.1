dc36299e9df6be10cb92f7276666a1a0a07e3921
Zlobryak <clychnik@gmail.com>
2023-02-16 05:30:47 +0300

    readme rename

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+        int year = 401;
+        YearsCounter service = new YearsCounter();
+        int leapYearCount = service.leapYearCount(year);
+        System.out.println("________");
+        System.out.println(leapYearCount);
+
+    }
 }
--- src/main/java/YearsCounter.java
+++ src/main/java/YearsCounter.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class YearsCounter {
+public class YearsCounter {
+    public static int leapYearCount(int year) {
+        int counter = 0;
+        for (double i = 1; i <= year; i++) {
+            if ((i / 4) % 1 == 0) {
+
+                if ((i / 100) % 1 != 0) {
+                    counter++;
+                }
+                if ((i / 400) % 1 == 0) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
+    }
 }
